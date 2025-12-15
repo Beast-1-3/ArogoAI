912796f06356c4b7dd91b81c2d8842b004d30419
Akarsh Singh <akarsh7376@gmail.com>
2025-12-16 00:09:32 +0530

    Update API URL and CORS for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   //baseURL: "http://localhost:5001/api",
-  baseURL: "https://arogo-backend.onrender.com/api",
+  baseURL: "https://arogoai-nn1e.onrender.com/api",
   withCredentials: true,
 });
 
